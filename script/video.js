ead05978e23b7a6fa5d3799369bc369a50a114f2
Talibouyadanfa7 <stg_danfa75294@orange-sonatel.com>
2024-02-08 17:46:21 +0000

    first commit

---
+++ script/video.js
@@ -0,0 +1,105 @@
+const jsonData = [
+    {
+        "imageSrc": "https://www.youtube.com/embed/yagbNj8xNFI",
+        "ribbonText": "Free",
+        "badges": [
+            {"text": "Design", "color": "primary"},
+            {"text": "Beginner", "color": "dark"}
+        ],
+        "title": "Comment créer une Entreprise individuelle au Sénégal",
+        "rating": {"value": 4.5, "count": 6500},
+        "time": {"duration": "6h 56m", "lectures": 82},
+        "user": {"avatarSrc": "assets/images/avatar/10.jpg", "name": "Larry Lawson"},
+        "price": "4000 FCFA"
+    },
+    {
+        "imageSrc": "https://www.youtube.com/embed/Viui3LuooLI",
+        "badges": [
+            {"text": "Development", "color": "primary"},
+            {"text": "All level", "color": "dark"}
+        ],
+        "title": "Comment vendre vos produits en ligne au Sénégal",
+        "rating": {"value": 4.0, "count": 3500},
+        "time": {"duration": "12h 45m", "lectures": 65},
+        "user": {"avatarSrc": "assets/images/avatar/04.jpg", "name": "Billy Vasquez"},
+        "price": "Gratuit"
+    },
+    {
+        "imageSrc": "https://www.youtube.com/embed/yagbNj8xNFI",
+        "badges": [
+            {"text": "Design", "color": "primary"},
+            {"text": "Beginner", "color": "dark"}
+        ],
+        "title": "Comment créer une Entreprise individuelle au Sénégal",
+        "rating": {"value": 4.5, "count": 2000},
+        "time": {"duration": "24h 56m", "lectures": 55},
+        "user": {"avatarSrc": "assets/images/avatar/09.jpg", "name": "Lori Stevens"},
+        "price": "3000 FCFA"
+    },
+    {
+        "imageSrc": "https://www.youtube.com/embed/Viui3LuooLI",
+        "badges": [
+            {"text": "Design", "color": "primary"},
+            {"text": "Beginner", "color": "dark"}
+        ],
+        "title": "Comment vendre vos produits en ligne au Sénégal",
+        "rating": {"value": 4.0, "count": 1200},
+        "time": {"duration": "09h 56m", "lectures": 21},
+        "user": {"avatarSrc": "assets/images/avatar/01.jpg", "name": "Frances Guerrero"},
+        "price": "2000 FCFA"
+    }
+]
+
+
+const container = document.querySelector('.tiny-slider-inner');
+
+for (const data of jsonData) {
+    const cardDiv = document.createElement('div');
+    cardDiv.innerHTML = `
+    <div class="card action-trigger-hover border bg-transparent">
+    <iframe width="670" height="320" class="card-img-top" src="${data.imageSrc}" title="Comment créer une Entreprise individuelle au Sénégal" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        
+      ${data.ribbonText ? `<div class="ribbon mt-3"><span>${data.ribbonText}</span></div>` : ''}
+      <div class="card-body pb-0">
+        <div class="d-flex justify-content-between  pe-5 mb-3">
+             <div class="d-flex ">
+            <div><a href="#" class="badge bg-${data.badges[0].color} bg-opacity-10 text-primary !important">${data.badges[0].text}</a></div>
+            <div class="mx-2"><a href="#" class="badge text-bg-dark  !important">${data.badges[1].text}</a></div>
+            </div>
+          <a href="#" class="h6 fw-light mb-0"><i class="far fa-bookmark"></i></a>
+        </div>
+        <h5 class="card-title"><a href="#">${data.title}</a></h5>
+        <div class="d-flex justify-content-between mb-2">
+          <div class="hstack gap-2">
+            <p class="text-warning m-0">${data.rating.value}<i class="fas fa-star text-warning ms-1"></i></p>
+            <span class="small">(${data.rating.count})</span>
+          </div>
+          <div class="hstack gap-2">
+            <p class="h6 fw-light mb-0 m-0">${data.rating.count}</p>
+            <span class="small">(Student)</span>
+          </div>
+        </div>
+        <div class="hstack gap-3">
+          <span class="h6 fw-light mb-0"><i class="far fa-clock text-danger me-2"></i>${data.time.duration}</span>
+          <span class="h6 fw-light mb-0"><i class="fas fa-table text-orange me-2"></i>${data.time.lectures} lectures</span>
+        </div>
+      </div>
+      <div class="card-footer pt-0 bg-transparent">
+        <hr>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="d-flex align-items-center">
+            <div class="avatar avatar-sm">
+              <img class="avatar-img rounded-1" src="${data.user.avatarSrc}" alt="avatar">
+            </div>
+            <p class="mb-0 ms-2"><a href="#" class="h6 fw-light mb-0">${data.user.name}</a></p>
+          </div>
+          <div>
+            <h4 class="text-success mb-0 item-show">${data.price}</h4>
+            <a href="#" class="btn btn-sm btn-success-soft item-show-hover"><i class="fas fa-shopping-cart me-2"></i>Add to cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
+    container.appendChild(cardDiv);
+}
--- server.js
+++ server.js
@@ -1,7 +1,78 @@
-const app = require('./app')
 
-const port = process.env.PORT || 3000
+const express = require('express');
+const path = require('path');
+const axios = require('axios');
+const app = express();
+const SignUpData = require('./model/signupData.js');
+const SignInData = require('./model/signupData.js');
 
+app.use(express.static(path.join(__dirname, 'bolya_front')));
+app.use('/script',express.static(path.join(__dirname, "script")));
+app.use(express.json()); // Middleware pour parser les requêtes JSON
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({ extended: true }));
+const port = process.env.PORT || 3000;
+
+const apiURL="http://localhost:3010/api/auth"
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'bolya_front/index.html'));
+});
+
+app.get('/signup', function(req, res) {
+    res.sendFile(path.join(__dirname, 'bolya_front/sign-up.html'));
+});
+
+app.get('/sign-in', function(req, res) {
+    res.sendFile(path.join(__dirname, 'bolya_front/sign-in.html'));
+});
+
+app.get('/home', function(req,res) {
+    res.sendFile(path.join(__dirname, 'bolya_front/index.html'));
+});
+
+// Endpoint pour la connexion (signin)
+app.post('/signin', async (req, res) => {
+    try {
+        // Récupérer les données du corps de la requête
+        const { phoneNumber, password } = req.body;
+        const signInData = new SignInData( phoneNumber, password);
+
+
+        // Effectuer une requête POST à l'API pour la connexion
+        const response = await axios.post(`${apiURL}/signup`, signInData);
+
+        // Si la connexion est réussie, renvoyer les données de l'utilisateur
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erreur lors de la connexion :', error.response.data);
+        // Renvoyer une erreur avec le code d'erreur de l'API
+        res.status(error.response.status).json(error.response.data);
+    }
+});
+
+// Endpoint pour l'inscription (signup)
+app.post('/signup', async (req, res) => {
+    try {
+        // Récupérer les données du corps de la requête
+
+        const {firstname,lastname, phoneNumber, password,accountChoice } = req.body;
+        console.log(req.body)
+        const signUpData = new SignUpData(firstname,lastname, phoneNumber, password,accountChoice);
+
+        // Effectuer une requête POST à l'API pour l'inscription
+        const response = await axios.post(`${apiURL}/signup`, signUpData);
+        console.log(1)
+        // Si l'inscription est réussie, renvoyer les données de l'utilisateur
+        res.json(response.data);
+    } catch (error) {
+        console.error('Erreur lors de l\'inscription dd :', error.response.data);
+        // Renvoyer une erreur avec le code d'erreur de l'API
+        res.status(error.response.status).json(error.response.data);
+    }
+});
+
+// Démarrez le serveur
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Server is up on port ${port}`);
+});
